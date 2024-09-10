564978f4392198c874454430071fe6f5d03e3d28
RajRao01 <unofficialraj01@gmail.com>
2024-09-10 11:03:09 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";//treat all js code as newer version
+
+// alert (3+3)// we are using nodejs,not browser
+
+console.log(3
+    +
+    3)//code readability should be high
+
+    console.log("vishal")
+
+    let name = "vishal"
+    let age = 18
+    let isLoggedIn = false
+    let state;
+    // number => 2 to power 53
+    // bigint
+    // string =>""
+    // boolean => true/false
+    // null => standalone value
+    // undefiened =>
+    // symbol => unique
+
+    // object
+     console.log(typeof "vishal");
+     console.log(typeof undefined);//undfined
+     console.log(typeof null)//object
