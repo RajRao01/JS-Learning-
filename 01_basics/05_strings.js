219ecd54dab675948ba65c9d01383975af8169ea
RajRao01 <unofficialraj01@gmail.com>
2024-09-12 11:10:33 +0000

    string

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,16 +1,34 @@
 const name = " vishal "
 const repoCount = 50
 
-console.log(name  + repoCount+ " Value");
-console.log(`Hello my name is ${name}and my repo count is ${repoCount} `);
+// console.log(name  + repoCount+ " Value");
+// console.log(`Hello my name is ${name}and my repo count is ${repoCount} `);
 
-const gameName = new String('vishal')
+ const gameName = new String('vishal-vc-com')
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
 
 
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('l'));
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+ console.log(gameName.charAt(2));
+ console.log(gameName.indexOf('l'));
+
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+const newStringOne = "     vishal    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://vishal.com/vishal%20rao"
+
+console.log(url.replace('%20' ,'-'))
+
+console.log(url.includes('sundar'))
+console.log(gameName.split('-'));
