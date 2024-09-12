3efefd49cefc4cc4bc98fb70f1c42dd1f2b59d88
RajRao01 <unofficialraj01@gmail.com>
2024-09-12 07:07:00 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+const name = " vishal "
+const repoCount = 50
+
+console.log(name  + repoCount+ " Value");
+console.log(`Hello my name is ${name}and my repo count is ${repoCount} `);
+
+const gameName = new String('vishal')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('l'));
