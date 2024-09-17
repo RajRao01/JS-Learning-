e33e8dbb75e135526f515e60a31ad4c28f5d2f09
RajRao01 <unofficialraj01@gmail.com>
2024-09-17 18:22:16 +0000

    filter using

---
+++ 05_iterations/six.js
@@ -0,0 +1,41 @@
+// const coding = ["js","ruby","java","python","cpp"]
+
+
+
+// const values = coding.forEach( (item) => {
+//     // console.log(item);
+//     return item
+// })
+
+// console.log(values);
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.filter( (num) => num > 4 )
+// const newNums = myNums.filter( (num) =>{
+//     return num > 4
+// })
+
+// const newNums = []
+// myNums.forEach( (num) => {
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// })
+// console.log(newNums);
+
+const books = [
+    {title : 'Book One', genre :  'Fiction', publish : 1981, edition : 2004},
+    {title :' Book Two', genre : 'Non-Fiction', publish : 1992, edition : 2008},
+    {title : 'Book three', genre : 'History', publish : 1999, edition : 2007},
+    {title : 'Book four', genre : 'Non-Fiction', publish : 1989, edition : 2010},
+    {title : 'Book five', genre : 'Science', publish : 2009, edition : 2014},
+    {title : 'Book six', genre : 'Fiction', publish : 1987, edition : 2007},
+    {title : 'Book seven', genre : 'History', publish : 1998, edition : 2009},
+];
+
+let userBooks = books.filter( (bk) => bk.genre === 'History')
+
+userBooks = books.filter( (bk) => {return bk.publish >= 1995 && bk.genre === 'History'})
+
+console.log(userBooks);
