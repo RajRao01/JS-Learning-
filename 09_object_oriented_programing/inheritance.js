bb9dbacec336e0872dac109695b87dcfe917a260
RajRao01 <unofficialraj01@gmail.com>
2024-09-24 08:28:35 +0000

    inheritance and staticprop

--- 09_object_oriented_programing/inheritance.js
+++ 09_object_oriented_programing/inheritance.js
@@ -15,11 +15,15 @@ class Teacher extends User{
         this.password = password
     }
 
-    addCource(){
+    addCourse(){
         console.log(`A new cource was added by ${this.username}`);
     }
 }
 
 const chai = new Teacher("chai","chai@gmail.com","123")
 
-chai.addCource()
+chai.logMe()
+const masalaChai = new User("masalaChai")
+
+masalaChai.logMe()
+console.log(chai instanceof User);
